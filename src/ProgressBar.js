46a345ecba879970cdd2cafca5fe13dfbf1e11f3
prasanthsamy <selvaprasanths.20mech@kongu.edu>
2024-05-24 18:26:00 +0530

    Optimized Progressbar

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -9,8 +9,9 @@ const ProgressBar = (arr) =>{
         var pro = 0;
         const t1 = setInterval(()=>{
             time1 = t1;
-            percentNum.current.innerText = i + "%";
-            progress.current.style.background = `conic-gradient(rgb(0, 123, 255) ${3.6*pro}deg,rgba(0, 0, 0, 0.775) 0deg)`
+            percentNum.current.innerHTML = i + "%";
+            // progress.current.style.background = `conic-gradient(rgb(0, 123, 255) ${3.6*pro}deg,rgba(0, 0, 0, 0.775) 0deg)`
+            progress.current.style.strokeDashoffset = `${(100 - pro) * 2.826}`;
             ++i;
             ++pro;
             if(i === arr.props.percent + 1){
@@ -30,9 +31,14 @@ const ProgressBar = (arr) =>{
         <div className="progressbar-container">
             <div className="rectangle-container">
                 <div className="wrap-progress">
-                    <div ref={progress} className="circle">
+                    {/* <div ref={progress} className="circle">
                         <div ref={percentNum} className="percent"></div>
-                    </div>
+                    </div> */}
+                    <svg viewBox='0 0 100 100' className='circle'>
+                        <circle className='static-circle' cx={"50%"} cy={"50%"} r={45}></circle>
+                        <circle ref={progress} className='dynamic-circle' cx={"50%"} cy={"50%"} r={45}></circle>
+                        <text ref={percentNum} x={"30%"} y={"58%"}></text>
+                    </svg>
                     <div className="inner-texts">
                         <div className="skillname">{arr.props.progName}</div>
                         <div className="skill-quote">
